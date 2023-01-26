7b4958b11f1b2f95cbe301bb324319153105f14d
THOMY NTIKALA LOWE <thomylowe@THOMYs-MacBook-Air.local>
2023-01-26 05:28:48 +0200

    done with ssr

---
+++ pages/events/index.js
@@ -0,0 +1,38 @@
+import Image from 'next/image';
+import Link from 'next/link';
+const EventsPage=({data})=>{
+    
+    return (
+        <div>
+            <h1>Event Page</h1>
+                 <div>
+
+                    {data.map((ev)=>(
+                        <Link key={ev.id} href={`/events/${ev.id}`} passHref legacyBehavior>
+                        <a >
+                            <Image width={300} height={200} alt={ev.title} src={ev.image} />
+                            <h2>{ev.title} </h2>
+                        </a>
+                        </Link>
+                    ))}
+                        
+                 </div>
+        </div>
+       
+    )
+}
+
+export default EventsPage;
+
+export async function getStaticProps(){
+
+    const {events_categories}= await import('/data/data.json')
+
+  //console.log(events_categories)
+   return{
+    props:{
+        data:events_categories,
+    }
+   }
+
+}
