7d693dd2eac455756ba69307eb992fc7ac999dd4
THOMY NTIKALA LOWE <thomylowe@THOMYs-MacBook-Air.local>
2023-01-27 05:15:29 +0200

    style first part

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,20 +1,35 @@
 import Link from "next/link"
+import Image from "next/image"
 export const Header=()=>{
     return (
         <header>
-        <nav>
-          <Link href="/" passHref legacyBehavior>
-            <a>Home</a>
-          </Link>
-          <Link href="/events" passHref legacyBehavior>
-            <a>Events</a>
-          </Link>
-          <Link href="/about-us" passHref legacyBehavior>
-            <a>About Us</a>
-          </Link>
-         
-          
-        </nav> 
+            <div className="topNav">
+                <Image src={'/images/logo_black.png'} alt="logo" width={50} height={50} />
+              <nav>
+                <ul>
+                    <li>
+                        <Link href="/" passHref legacyBehavior>
+                            <a>Home</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/events" passHref legacyBehavior>
+                          <a>Events</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/about-us" passHref legacyBehavior>
+                            <a>About Us</a>
+                        </Link>
+                    </li>
+                </ul>
+               
+                
+                
+              </nav> 
+              
+            </div>
+            <h1> Sed ut perspiciatis unde omnis</h1>
       </header>
     )
 }
--- src/components/home/home-page.jsx
+++ src/components/home/home-page.jsx
@@ -2,15 +2,23 @@ import Image from "next/image"
 import Link from "next/link"
 export const HomePage=({data})=>{
     return (
-  <main >
+  <div className="home-body">
     {data.map((ev)=>(
-      <Link key={ev.id} href={`/events/${ev.id}`} passHref legacyBehavior>
-    <a >
-       <Image alt={ev.title} width={200} height={150} src={ev.image}/> 
-      <h2>{ev.title} </h2>
-      <p>{ev.description} </p> 
+      <Link  key={ev.id} href={`/events/${ev.id}`} passHref legacyBehavior>
+    <a className="card" href={`/events/${ev.id}`} >
+      <div className="image">
+         <Image alt={ev.title} width={480} height={300} src={ev.image}/> 
+      </div>
+       
+       <div className="content">
+           <h2>{ev.title} </h2>
+           <br/>
+           <p>{ev.description} </p> 
+       </div>
+      
     </a>
     </Link>
     ))}
-  </main>)
+  </div>
+  )
 }
